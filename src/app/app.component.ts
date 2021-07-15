4cf3f219d4456be2df6e6bbbc6f1a623af450902
Samuel10F <samueldf7994@gmail.com>
2021-07-15 09:31:11 +0200

    Added basic header and footer and testing build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'; 
+import { Component, OnInit } from '@angular/core';
+import { faHome, faIdCard } from '@fortawesome/free-solid-svg-icons'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  faCoffee = faCoffee;
+export class AppComponent implements OnInit{
+  faHome = faHome;
+  faIdCard = faIdCard;
   title = 'samuel10f';
+  private underline;
+  private oldClass;
+
+  public navigationLinks = [
+    {title: 'Proyectos', routerLink: '/proyectos'},
+    {title: 'Blog', routerLink: '/blog'},
+    {title: 'Contacto', routerLink: '/contacto'}
+  ]
+
+  private test = ['left', 'center', 'right'];
+
+  ngOnInit(): void {
+    this.underline = document.getElementById('LinkActiveUnderline');
+  }
+
+  setLinkHoverAnimation(linkIndex: number) {
+    
+    // let linkWidth = 151  * linkIndex;
+    // this.underline.style.left = `${linkWidth}px`;
+    this.underline.classList.remove(this.oldClass);
+    this.oldClass = this.test[linkIndex]
+    this.underline.classList.add(this.test[linkIndex])
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,29 @@
 module.exports = {
-  purge: ["./src/**/*.{html,ts}"],
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       gridTemplateColumns: {
-        root: "80px minmax(900px, 1fr)",
+        header: "repeat(3, minmax(00px, 1fr))",
+        headerMobile: "repeat(2, minmax(100px, 1fr))",
       },
+      gridTemplateRows: {
+        header: "auto 20px",
+        headerMobile: "40px 40px",
+      },
+    },
+    fontFamily: {
+      assistant: ["Assistant", "sans-serif"],
+    },
+    letterSpacing: {
+      widest: ".15em",
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      fontSize: ["hover"],
+      borderWidth: ["hover"],
+    },
   },
   plugins: [],
 };
